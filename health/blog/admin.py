f657c9a1cb9bb78c01a9c3368e2c19fad5f862ff
AlexMuia31 <alexmuia31@gmail.com>
2020-09-26 21:06:10 +0300

    issues fixed

--- health/blog/admin.py
+++ health/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Blog
-from django.contrib.auth.models import Group
+
 
 # Register your models here.
 admin.site.site_header ='Community Health Admin' #changes the header of the admin page
