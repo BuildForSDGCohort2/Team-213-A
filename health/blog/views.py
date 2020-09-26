bfc8ca231782e6dafca934549da9e753c565ad24
AlexMuia31 <alexmuia31@gmail.com>
2020-09-26 20:45:28 +0300

    blog and chat

---
+++ health/blog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView ,DetailView
+from . models import Blog
+
+# Create your views here.
+class BlogView(ListView):
+    
+    queryset = Blog.objects.order_by('-pub_date')
+    template_name='blog/blog.html'
+    paginate_by = 5
+
+
+
+class BlogDetail(DetailView):
+    model= Blog
+    template_name='blog/blog_detail.html'  
+
