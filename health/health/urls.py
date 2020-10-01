d041260e2e0c7248c1e2faa6fbdda5248fe88ec9
mwinamijr <mwinamijr@gmail.com>
2020-10-01 13:50:33 +0300

    added django rest framework

--- health/health/urls.py
+++ health/health/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('accounts.urls')),
     path('chat/', include('chat.urls')),
     path('blog/', include('blog.urls')),
