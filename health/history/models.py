1c61d1eec11fc7a0de1d43ec98cd6547b37ec3f4
mwinamijr <mwinamijr@gmail.com>
2020-10-01 13:43:00 +0300

    created patient model

--- health/history/models.py
+++ health/history/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PatientHistory(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    age = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=25, blank=True, null=True)
+    history_of_presenting_illness = models.TextField(blank=True)
+    past_medical_history = models.TextField(blank=True,
+        null=True)
+    medication_history = models.TextField(blank=True,
+        null=True)
+    family_history = models.TextField(blank=True,
+        null=True)
+    social_history = models.TextField(blank=True,
+        null=True)
+
+    def __str__(self):
+        return f"{self.patient}"
