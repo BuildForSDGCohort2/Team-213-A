4204bbd6b5f30614a85c7877a39f7656f8dc7747
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-10-02 13:11:50 +0300

    Re-structure models.

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,29 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
+from tinymce import HTMLField
+
+
+class Category(models.Model):
+    """Blog Post Categories."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
     """Blog Article posted by signed-in users."""
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    title = models.CharField(
-        max_length=100, null=False,
-        blank=False, unique=True)
-    body = models.TextField(null=False, blank=False)
-    image = models.ImageField(
-        upload_to='blog/images/%Y/%m/',
-        null=True, blank=True)
+    title = models.CharField(max_length=100, blank=False)
+    headline = models.CharField(max_length=255, blank=False)
     pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, default="General")
+    body = HTMLField('Body Content')
+    is_published = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ['-pub_date', 'author', 'is_published']
 
     def __str__(self):
         return self.title
-
-    def summary(self):
-        return self.body[:150]
