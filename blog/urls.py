d04134e25f03b2c4fb002f42053b4b31a387acbf
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-10-04 17:00:34 +0300

    Design Blog UI.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import (ArticleView, ArticleDetail)
 
 app_name = 'blog'
 urlpatterns = [
-    path('', ArticleView.as_view(), name='home'),
-    path('<pk>/', ArticleDetail.as_view(), name='article')
+    path('articles/', ArticleView.as_view(), name='index'),
+    path('articles/<pk>/', ArticleDetail.as_view(), name='article'),
 ]
